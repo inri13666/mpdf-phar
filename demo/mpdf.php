933d2724729b7a0c70940b0b62b78ca9dd311066
Nikita Makarov <Nikita.Makarov@effective-soft.com>
2014-03-14 14:03:07 +0300

    v0.0.1

---
+++ demo/mpdf.php
@@ -0,0 +1,3 @@
+<?php
+define('_MPDF_URI',__DIR__);
+require_once 'phar://' . dirname(dirname(__FILE__)) . '/compiled/mpdf.bz2';
---
+++ phar-bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+define('DS', DIRECTORY_SEPARATOR);
+if (version_compare(PHP_VERSION, '5.3.0') < 0) {
+    exit("PHP must be 5.3.0+");
+}
+Phar::mapPhar();
+/**
+ * Default Temp Dir
+ */
+if (!defined('_MPDF_TEMP_PATH')) define("_MPDF_TEMP_PATH", sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'tmp/');
+if (!defined('_MPDF_URI')) {
+    //define('_MPDF_URI', _MPDF_PATH);
+    exit('Please Define Public mPDF folder');
+}
+$basePath = 'phar://' . __FILE__ . '/';
+require_once $basePath . 'mpdf.php';
+__HALT_COMPILER();
+?>
