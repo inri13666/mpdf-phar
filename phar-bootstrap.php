d12e7e24ccb68351c9971615b4a5a9522c929c89
Nikita Makarov <Nikita.Makarov@effective-soft.com>
2014-03-17 15:43:52 +0300

    DS Notice Fixed

--- phar-bootstrap.php
+++ phar-bootstrap.php
@@ -8,6 +8,8 @@ Phar::mapPhar();
  * Default Temp Dir
  */
 if (!defined('_MPDF_TEMP_PATH')) define("_MPDF_TEMP_PATH", sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'tmp/');
+//TODO this
+// sys_get_temp_dir() => if (!defined('_MPDF_TTFONTDATAPATH')) { define('_MPDF_TTFONTDATAPATH',_MPDF_PATH.'ttfontdata/'); }
 if (!defined('_MPDF_URI')) {
     //define('_MPDF_URI', _MPDF_PATH);
     exit('Please Define Public mPDF folder');
